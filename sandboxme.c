5109ea3af72745f941a5d2765e45d5e2dcdf4c34
Julien Tinnes <jln@google.com>
2012-01-03 11:22:25 -0800

    Add proper license information

--- sandboxme.c
+++ sandboxme.c
@@ -1,11 +1,22 @@
-/* Work in progress, Julien Tinnes <jln@google.com>
+/* Copyright 2009 Google Inc. All Rights Reserved.
  *
- * With ideas and feedback from Tavis Ormandy and Chris Evans
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Julien Tinnes
  *
  * This sandbox could be dangerous and lower the security of the system if you
  * don't know what you're doing! Read the README file and be careful.
  */
-
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
